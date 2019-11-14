456b39046066f3146e8840fe911afae50352418d
qiaokuahai <3070993883@qq.com>
2019-11-14 14:27:22 +0800

    新增： 新增查找全部元素方法

--- demo/scripts/fetch_jinja2_element.py
+++ demo/scripts/fetch_jinja2_element.py
@@ -1,9 +1,9 @@
 from demo.scripts import get_file_dir_path
 import re
-import os
 
 
 def striptags(m):
+    # 将jinja2语法中的 openxml标签全部移除
     res = re.sub('</w:t>.*?(<w:t>|<w:t [^>]*>)',
                  '',
                  m.group(0),
@@ -25,11 +25,10 @@ def get_content(path):
 
 path = get_file_dir_path()
 path = path + "resume_tpl.txt"
-
 src_xml = get_content(path)
 
 
-def replace_ele(src_xml):
+def sub_ele(src_xml):
     src_xml = re.sub(r'{%(?:(?!%}).)*|{{(?:(?!}}).)*',
                      striptags,
                      src_xml,
@@ -38,15 +37,29 @@ def replace_ele(src_xml):
 
 
 def search_ele(src_xml):
-    res = re.search(r'{%(?:(?!%}).)*|{{(?:(?!}}).)*', src_xml, re.M)
+    res = re.search(r'{%(?:(?!%}).)*|{{(?:(?!}}).)*', src_xml)
+    print(res.group())
+
+
+def match_ele(src_xml):
+    res = re.match(r'{%(?:(?!%}).)*|{{(?:(?!}}).)*', src_xml)
+    if res is None:
+        print("未匹配到数据")
+        return
     print(res.group())
 
 
 def findall_ele(src_xml):
-    pass
+    # 搜索所有{{ }}, {% %}之间的内容，两边括号包含头，不包含尾，例如{% endfor
+    res = re.findall(r'{%(?:(?!%}).)*|{{(?:(?!}}).)*', src_xml)
+    # 如果匹配到，则将元素放入list中，如果一个都没有匹配到，则列表为空
+    print(res)
+    for m in res:
+        print(m)
 
 
-search_ele(src_xml)
+if __name__ == "__main__":
+    findall_ele(src_xml)
 
 
 
